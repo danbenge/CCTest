4b98bf628cfaea5b8e64f9837d2af4ceefb9b0ab
Dan Benge <dan.benge@gmail.com>
2012-08-03 15:53:51 -0400

    Made a change

--- CCTest/Program.cs
+++ CCTest/Program.cs
@@ -9,7 +9,7 @@ namespace CCTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Whoo hoo!");
         }
     }
 }
